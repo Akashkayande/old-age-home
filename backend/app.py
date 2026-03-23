4abc63007c73026420d3e020dba2552e7268502a
Akashkayande <ramdaskayande637@gmail.com>
2026-03-23 10:56:16 +0530

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from routes.auth import auth_bp
 from flask_cors import CORS
 app = Flask(__name__)
@@ -10,4 +11,5 @@ def home():
     return "Backend is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
